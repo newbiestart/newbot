82bc90c53cdfdccb2e3199a52bf94a9ad5c3a32a
newbiestart <ptmaevim@gmail.com>
2025-04-02 20:38:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Salom! Men sizning yangi botingizman!")
+    bot.send_message(message, "Salom! Men sizning yangi botingizman!")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(message, message.text)
 
 print("Bot ishga tushdi...")
 bot.polling()
